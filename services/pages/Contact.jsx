600b6f0dac0ce880640261591116d77eaaa06c6f
Twinklekumari2 <twinklekumari246@gmail.com>
2025-12-29 03:18:35 +0530

    added correct link

--- services/pages/Contact.jsx
+++ services/pages/Contact.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Navbar from '../src/components/Navbar'
+<Navbar/>
+import CTA from '../src/components/CTA'
+import Footer from '../src/components/Footer'
 
 const Contact = () => {
   return (
